ff42f5e0dd579b949c206d86614657507b7ebe8b
Lovisgod <olifedayo94@gmail.com>
2020-06-02 17:04:46 +0100

    feature: media notification

--- app/src/main/java/com/kodehauz/radiobasar/MainActivity.kt
+++ app/src/main/java/com/kodehauz/radiobasar/MainActivity.kt
@@ -1,52 +1,67 @@
 package com.kodehauz.radiobasar
 
-import android.app.Dialog
-import android.app.ProgressDialog
-import android.media.AudioManager
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.graphics.Color
 import android.media.MediaPlayer
+import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
 import com.github.loadingview.LoadingDialog
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import com.kodehauz.radiobasar.utils.OnClearFromRecentService
+import com.kodehauz.radiobasar.utils.Playable
+import com.kodehauz.radiobasar.utils.cancalNotifications
+import com.kodehauz.radiobasar.utils.sendNotification
 import java.io.IOException
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Playable {
     private lateinit var player: MediaPlayer
     private var playing: Boolean = false
+    private lateinit var playButton: Button
+    val ACTION_PLAY = "PLAY"
+    val ACTION_PAUSE = "PAUSE"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        createChannel(
+            getString(R.string.radio_notification_channel_id),
+            getString(R.string.egg_notification_channel_name)
+        )
+        registerReceiver(broadcastReceiver, IntentFilter("TRACK TRACK"))
+        startService(Intent(baseContext, OnClearFromRecentService::class.java))
         initializeMediaPlayer()
-        val playButton: Button = findViewById(R.id.play)
+        showNotification()
+        playButton = findViewById(R.id.play)
         playButton.setOnClickListener {
             if (playing) {
                 pausePlaying()
                 playing = false
                 println(playing)
-                playButton.text  = "Play"
             } else {
                 startPlaying()
                 playing = true
                 println(playing)
-                playButton.text = "Stop"
             }
         }
     }
 
     private fun startPlaying() {
         player.start()
+        playButton.text = "Stop"
     }
 
     private fun pausePlaying() {
         if (player.isPlaying) {
             player.pause()
+            playButton.text  = "Play"
         }
     }
 
@@ -73,6 +88,37 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onPlay() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationManager = this@MainActivity.getSystemService(
+                NotificationManager::class.java
+            )
+            notificationManager?.sendNotification(
+                "hello",
+                this@MainActivity,
+                R.drawable.ic_pause_black_24dp
+            )
+        }
+        startPlaying()
+        playing = true
+    }
+
+    override fun onTrackPause() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationManager = this@MainActivity.getSystemService(
+                NotificationManager::class.java
+            )
+            notificationManager?.sendNotification(
+                "hello",
+                this@MainActivity,
+                R.drawable.ic_play_arrow_black_24dp
+            )
+        }
+
+       pausePlaying()
+       playing = false
+    }
+
     override fun onPause() {
         super.onPause()
         if (player.isPlaying) {
@@ -92,6 +138,73 @@ class MainActivity : AppCompatActivity() {
         super.onStart()
 
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationManager = this.getSystemService(
+                NotificationManager::class.java
+            )
+            notificationManager?.cancalNotifications()
+        }
+        unregisterReceiver(broadcastReceiver)
+    }
+
+    private fun createChannel(channelId: String, channelName: String) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                channelId,
+                channelName,
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.RED
+            notificationChannel.enableVibration(true)
+            notificationChannel.description = "Listening to radio"
+
+            val notificationManager = this.getSystemService(
+                NotificationManager::class.java
+            )
+            notificationManager?.createNotificationChannel(notificationChannel)
+        }
+
+
+    }
+
+
+    private fun showNotification() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationManager = this.getSystemService(
+                NotificationManager::class.java
+            )
+            notificationManager?.sendNotification(
+                "Listen to life",
+                this,
+                R.drawable.ic_play_arrow_black_24dp
+            )
+        }
+    }
+
+
+    var broadcastReceiver: BroadcastReceiver =
+        object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+                when(intent.getStringExtra("playpause")) {
+                    ACTION_PAUSE -> {
+                        onTrackPause()
+                    }
+
+                    ACTION_PLAY -> {
+                        if (playing) {
+                            onTrackPause()
+                        } else {
+                            onPlay()
+                        }
+
+                    }
+                }
+            }
+        }
 
 
 }
---
+++ app/src/main/java/com/kodehauz/radiobasar/utils/NotificationUtils.kt
@@ -0,0 +1,102 @@
+package com.kodehauz.radiobasar.utils
+
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.support.v4.media.session.MediaSessionCompat
+import androidx.core.app.NotificationCompat
+import com.kodehauz.radiobasar.MainActivity
+import com.kodehauz.radiobasar.R
+import java.lang.Exception
+
+// Notification ID.
+private val NOTIFICATION_ID = 2
+private val REQUEST_CODE = 0
+private val FLAGS = 0
+private val ACTION_PLAY = "PLAY"
+private val ACTION_PAUSE = "PAUSE"
+
+
+// TODO: Step 1.1 extension function to send messages (GIVEN)
+/**
+ * Builds and delivers the notification.
+ *
+ * @param context, activity context.
+ */
+
+// this is extension function from the notification class that gives freedom to perform different operation with
+// the notification
+fun NotificationManager.sendNotification(messageBody: String, applicationContext: Context, playBtn:Int) {
+    try {
+        val mediaSession = MediaSessionCompat(applicationContext, "tag")
+
+        // handle play action
+        val intentPlay = Intent(applicationContext, NotificationActionService::class.java)
+            .setAction("PLAY")
+        val pendingIntentPlay  = PendingIntent.getBroadcast(
+            applicationContext,
+            REQUEST_CODE,
+            intentPlay,
+            PendingIntent.FLAG_UPDATE_CURRENT)
+
+
+        // Create the content intent for the notification, which launches
+        // this activity
+        //  create intent
+        val contentIntent = Intent(applicationContext, MainActivity::class.java)
+        //  create PendingIntent
+        val contentPendingIntent = PendingIntent.getActivity(
+            applicationContext,
+            NOTIFICATION_ID,
+            contentIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        // add style
+
+        val radioImage =  BitmapFactory.decodeResource(
+            applicationContext.resources,
+            R.drawable.man_
+        )
+
+        //  get an instance of NotificationCompat.Builder
+        // Build the notification
+        val builder = NotificationCompat.Builder(
+            applicationContext, applicationContext.resources.getString(R.string.radio_notification_channel_id)
+        )
+            .setSmallIcon(R.drawable.ic_radio_icons_01)
+            .setLargeIcon(radioImage)
+            .setContentTitle(applicationContext.
+            resources.getString(R.string.notification_title))
+            .setContentText(messageBody)
+//            .setShowWhen(false)
+            .setContentIntent(contentPendingIntent)
+            .setOnlyAlertOnce(true)
+            .addAction(
+                playBtn,
+                applicationContext.getString(R.string.play),
+                pendingIntentPlay
+            )
+            .setStyle(androidx.media.app.NotificationCompat.MediaStyle()
+                .setShowActionsInCompactView(0)
+                .setMediaSession(mediaSession.sessionToken))
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+
+        // add play action
+
+
+
+        // nptify gets two parameters the notification id and the builder
+        notify(NOTIFICATION_ID, builder.build())
+    } catch (e:Exception) {
+        println("this is the error ${e.localizedMessage}")
+    }
+}
+
+// Cancel all notifications
+
+fun NotificationManager.cancalNotifications() {
+    cancelAll()
+}
